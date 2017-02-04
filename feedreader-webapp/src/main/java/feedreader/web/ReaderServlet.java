cd3b52dd670a1b00eee65f415330ab23046a1fd6
Jared Pearson <jaredapearson@gmail.com>
2017-02-04 07:04:41 -0800

    Add examples to the homepage

--- feedreader-webapp/src/main/java/feedreader/web/ReaderServlet.java
+++ feedreader-webapp/src/main/java/feedreader/web/ReaderServlet.java
@@ -1,20 +1,25 @@
 package feedreader.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ReaderServlet extends HttpServlet {
-	private static final long serialVersionUID = -217367977340304415L;
-	
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		request.getRequestDispatcher("/WEB-INF/views/reader.jsp").forward(request, response);
-		
-	}
+    private static final long serialVersionUID = -217367977340304415L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cookie cookie = Arrays.stream(request.getCookies())
+                .filter((c) -> c.getName().equals(AuthorizationFilter.SESSION_ID_COOKIE_NAME))
+                .findFirst()
+                .get();
+        request.setAttribute("sid", cookie.getValue());
+        request.getRequestDispatcher("/WEB-INF/views/reader.jsp").forward(request, response);
+    }
 }
--- feedreader-webapp/src/main/webapp/WEB-INF/views/reader.jsp
+++ feedreader-webapp/src/main/webapp/WEB-INF/views/reader.jsp
@@ -1,6 +1,40 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>FeedReader</title>
+<link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+<style>
+body {
+    font-family: 'Nunito Sans', sans-serif;
+    margin: 0;
+    padding-left: 1em;
+    padding-top: 10px;
+    padding-right: 1em;
+}
+.codeblock {
+    background-color: #eaeaea;
+    padding: 1em;
+    font-family: monospace;
+}
+</style>
+<head>
 <body>
-FeedReader
+<h1>FeedReader</h1>
+<p>This website is primarily REST based application. Below are some instructions on how to get started.</p>
+<p>
+    Your Session ID: ${sid}
+</p>
+<h2>Examples</h2>
+<h3>Viewing the stream</h3>
+<p>Gets a stream, which is a combination of all RSS feeds to read at once.</p>
+<div class="codeblock">
+    curl http://localhost:8080/services/v1/stream -H "Authorization:SID ${sid}"
+</div>
+
+<h3>Adding a RSS Stream</h3>
+<p>To request a new feed be added to the stream:</p>
+<div class="codeblock">
+    curl http://localhost:8080/services/v1/feedSubscriptions -H "Authorization:SID ${sid}" -X POST -H "Content-Type: application/json" -d '{"url":"http://www.nasa.gov/rss/dyn/breaking_news.rss"}'
+</div>
 </body>
 </html>
